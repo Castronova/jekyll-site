a9cf1909e9cc7e3a7da48d09a14864c00d271db0
Tony Castronova <castronova.anthony@gmail.com>
2015-11-06 10:01:39 -0700

    added missing files

---
+++ _site/config/deploy/production.rb
@@ -0,0 +1,32 @@
+set :stage, :production
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server definition into the
+# server list. The second argument is a, or duck-types, Hash and is
+# used to set extended properties on the server.
+
+ask(:password, nil, echo:false)
+server '129.123.51.178', user: 'castro', port: 22, roles: %w{web app}
+
+set :bundle_binstubs, nil
+
+set :bundle_flags, '--deployment --quiet'
+set :rvm_type, :user
+
+
+SSHKit.config.command_map[:rake]  = "bundle exec rake"
+SSHKit.config.command_map[:rails] = "bundle exec rails"
+
+namespace :deploy do
+
+  desc "Restart application"
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # execute :touch, release_path.join("tmp/restart.txt")
+    end
+  end
+
+  after :finishing, "deploy:cleanup"
+
+end
---
+++ config/deploy.rb
@@ -0,0 +1,41 @@
+# config valid only for Capistrano 3.1
+# lock '3.2.1'
+
+set :application, 'jekyll-site'
+set :repo_url, "git@github.com:Castronova/jekyll-site.git"
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, "/home/castro/Documents/#{fetch(:application)}"
+
+# Default value for :scm is :git
+set :scm, :git
+
+# Default value for :log_level is :debug
+set :log_level, :debug
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+  end
+end
+ 
+before :restart, :build_public do
+   on roles(:app) do
+     within "#{deploy_to}/current" do
+        #release_path do
+        #execute 'jekyll',  "build --destination public"
+        execute 'jekyll', "clean"
+        execute 'jekyll',  "build"
+      end
+   end
+end
+after :publishing, :restart
+
+end
